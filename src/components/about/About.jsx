0507f83b90e1a706677e65335a09400c05335813
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-06-19 00:20:14 +0300

    about component template redesign

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,32 +1,85 @@
-import './about.css'
-import aboutImg from '../../assets/images/about.jpg'
-import CV from '../../assets/cv/cv.pdf'
-import Info from './info';
-import SvgFiles from '../../assets/icons/Files';
+import aboutImg from "../../assets/images/web-developer.svg";
+import CV from "../../assets/cv/cv.pdf";
+import ButtonLink from "../buttons/ButtonLink";
+import { motion } from "framer-motion";
 
 function About() {
-    return ( 
-        <section className="about section" id="about">
-            <h2 className="section__title">About me</h2>
-            <span className="section__subtitle">My introduction</span>
+  const textAnimation = {
+    hidden: {
+      y: 100,
+      opacity: 0,
+    },
+    visible: (custom) => ({
+      y: 0,
+      opacity: 1,
+      transition: { delay: custom * 0.2, duration: 0.5 },
+    }),
+  };
 
-            <div className="about__container container grid">
-                <img src={aboutImg} alt="about image" className='about__img'/>
-                <div className="about__data">
-                    <Info/>
+  return (
+    <motion.section
+      className="section flex flex-col gap-16 py-16"
+      id="about"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 1, once: true }}
+    >
+      <motion.h2
+        className="text-center font-medium text-4xl"
+        custom={1}
+        variants={textAnimation}
+      >
+        Обо <span className="text-accent-clr">мне</span>
+      </motion.h2>
 
-                    <p className="about__description">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolore assumenda ullam quasi inventore sapiente, quibusdam nemo commodi tenetur dicta iusto doloremque quisquam! Dolorum non eum sint voluptatum tempore velit.
-                    </p>
+      <div className="custom-container flex flex-wrap gap-10">
+        <motion.div
+          className="flex-1 flex items-center justify-center"
+          custom={2}
+          variants={textAnimation}
+        >
+          <img
+            src={aboutImg}
+            alt="about image"
+            className="size-[350px] min-w-[250px] object-contain "
+          />
+        </motion.div>
 
-                    <a download="" href={CV} className="button button--flex">
-                        Download CV
-                        <SvgFiles/>
-                    </a>
-                </div>
-            </div>
-        </section>
-    );
+        <motion.div
+          className="flex-1 p-4 flex flex-col gap-8 justify-center"
+          custom={3}
+          variants={textAnimation}
+        >
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            dolore assumenda ullam quasi inventore sapiente, quibusdam nemo
+            commodi tenetur dicta iusto doloremque quisquam! Dolorum non eum
+            sint voluptatum tempore velit.
+          </p>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            dolore assumenda ullam quasi inventore sapiente, quibusdam nemo
+            commodi tenetur dicta iusto doloremque quisquam! Dolorum non eum
+            sint voluptatum tempore velit.
+          </p>
+          <motion.div
+            className="flex gap-4"
+            custom={4}
+            variants={textAnimation}
+          >
+            <ButtonLink href={CV} ext={true}>
+              <i className="bx bx-file"></i>
+              Резюме
+            </ButtonLink>
+            <ButtonLink href={"https://github.com/Dimar1510"} ext={true}>
+              <i className="devicon-github-original"></i>
+              Github
+            </ButtonLink>
+          </motion.div>
+        </motion.div>
+      </div>
+    </motion.section>
+  );
 }
 
 export default About;
