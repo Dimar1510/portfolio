3628d74326e29ea899177559db9630b4e984b8e9
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-08-02 18:10:48 +0300

    project card

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -12,16 +12,16 @@ const ProjectCard: FC<IProject> = ({
 }) => {
   return (
     <div
-      className="flex flex-col items-center justify-center gap-4 w-full border border-gray-50 p-4 rounded-lg shadow-[rgba(0,0,0,0.2)_2px_2px_6px_4px] transition-transform duration-500 will-change-transform
+      className="flex flex-col items-center justify-center gap-4 w-full border border-gray-50 p-3 rounded-lg shadow-[rgba(0,0,0,0.2)_2px_2px_6px_4px] transition-transform duration-500 will-change-transform
       hover:-translate-y-2 max-w-[400px] mx-auto"
     >
       <img
         loading="lazy"
         src={img}
         alt={title}
-        className="w-[320px] h-[240px] object-cover rounded-md border border-gray-500"
+        className="min-w-[250px] w-full h-[240px] object-cover rounded-md border border-gray-500"
       />
-      <div className="flex flex-col gap-4 flex-1">
+      <div className="flex flex-col gap-4 flex-1 w-full">
         <div className="flex flex-wrap gap-2 ">
           {tags.map((tag, index) => (
             <div key={index} className="border border-text-clr px-2 rounded-md">
@@ -29,8 +29,10 @@ const ProjectCard: FC<IProject> = ({
             </div>
           ))}
         </div>
+
         <h3 className="font-semibold text-2xl">{title}</h3>
         <p className="flex-1">{desc}</p>
+
         <div className="flex gap-2 justify-between w-full">
           <ButtonLink href={live} ext={true}>
             <div className="flex gap-1 items-center">
