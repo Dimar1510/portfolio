65dbb70f16f638f6b81d6e69552b726b443bf5f9
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-08-05 13:26:15 +0300

    padding

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -32,7 +32,7 @@ const Portfolio = () => {
       className="py-32 laptop:pt-12 tablet:pb-4"
       ref={ref}
     >
-      <div className="sticky top-0 left-0 pt-16 pb-4 text-center bg-body-clr z-10 transition-[background-color] duration-500">
+      <div className="sticky top-0 left-0 pt-20 pb-4 text-center bg-body-clr z-10 transition-[background-color] duration-500">
         <motion.h2
           custom={1}
           variants={textAnimation}
